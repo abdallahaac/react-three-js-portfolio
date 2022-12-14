88071777738257617986a88289b8fc6744e31275
abdallahaac <aceydesignz@gmail.com>
2022-12-14 12:16:23 -0500

    added routes

--- my-portfolio/src/Components/App.js
+++ my-portfolio/src/Components/App.js
@@ -1,18 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Art from "../Pages/Art";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import GraphicDesign from "../Pages/GraphicDesign";
+import Art from "../Pages/Art";
 import Home from "../Pages/Home";
+
 import TechnicalProjects from "../Pages/TechnicalProjects";
+
 import Navbar from "./Navbar";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+
 			<Routes>
-				<Route path="/" element={<Home></Home>} />
+				<Route path="/" element={<Home />} />
 				<Route path="/technical-projects" element={<TechnicalProjects />} />
 				<Route path="/graphic-design" element={<GraphicDesign />} />
+				<Route path="/art" element={<Art />} />
 				<Route path="/art" element={<Art />} />
 			</Routes>
 		</>
